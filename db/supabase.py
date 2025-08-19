0da66c6d84094f0e7fcd396534f8715c0128004f
YangQF2002 <yangqf18@gmail.com>
2025-08-19 11:48:03 +0800

    Minor db client fix

--- db/supabase.py
+++ db/supabase.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from supabase import Client, acreate_client, create_client
+from supabase import AClient, acreate_client
 
 load_dotenv()
 
@@ -9,11 +9,7 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 
 
-# Our client object that links to the supabase db
-supabase: Client = create_client(url, key)
-
-
-# New method to prevent disconnects?
+# New method to prevent disconnects
 async def get_supabase_client():
-    supabase: Client = await acreate_client(url, key)
+    supabase: AClient = await acreate_client(url, key)
     return supabase
